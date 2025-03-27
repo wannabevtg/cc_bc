2b5602f254b1f2ea212134fa69e04fe4b21bb19e
wannabevtg <viveksdharewa@gmail.com>
2025-03-27 12:04:06 +0530

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,17 +1,16 @@
 const hre = require("hardhat");
 
 async function main() {
-  // 1. Get the contract factory
+
   const LandTransferSystem = await hre.ethers.getContractFactory("LandTransferSystem");
   
-  // 2. Deploy the contract
+  
   const contract = await LandTransferSystem.deploy();
   console.log("Deployment transaction hash:", contract.deploymentTransaction().hash);
   
-  // 3. Wait for deployment confirmation
+ 
   await contract.waitForDeployment();
   
-  // 4. Get the final deployed address
   const deployedAddress = await contract.getAddress();
   console.log("Deployed to:", deployedAddress);
   
